c236a1a6b7a7b47b7cf9d91c7b8ffbef8466023f
Paul Liu <pstar.liu@gmail.com>
2012-03-25 17:34:18 +1300

    half way though scenarios

--- features/support/paths.rb
+++ features/support/paths.rb
@@ -17,6 +17,8 @@ module NavigationHelpers
       '/'
     when /^the edit page for "([^"]*)"$/
       edit_movie_path(Movie.find_by_title($1))
+    when /^the details page for "([^"]*)"$/
+      movie_path(Movie.find_by_title($1))
       # edit_movie_path(Movie.find_by_title("Alien"))
     # Add more mappings here.
     # Here is an example that pulls values out of the Regexp:
