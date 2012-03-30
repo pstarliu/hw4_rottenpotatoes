0de6876ebdd981db8d5e92ff6726d8489f4c633e
Paul Liu <pstar.liu@gmail.com>
2012-03-30 23:31:11 +1300

    Create rspec still not working

--- app/controllers/movies_controller.rb
+++ app/controllers/movies_controller.rb
@@ -48,7 +48,6 @@ class MoviesController < ApplicationController
   def create
     @movie = Movie.create!(params[:movie])
     flash[:notice] = "#{@movie.title} was successfully created."
-
     redirect_to movies_path
   end
 
